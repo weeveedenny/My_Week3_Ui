e58a4636dabb364881d0a7ae22e04773eb4c5dd0
weeveedenny <dennisodalonu@gmail.com>
2021-11-03 16:53:55 +0100

    Second commit
    more coming.

--- app/src/main/java/com/example/my_week2_ui/MainActivity.kt
+++ app/src/main/java/com/example/my_week2_ui/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.my_week2_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+
     }
 }
